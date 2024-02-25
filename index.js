3f5276d982cd53bb6a0e4c05b5ecfdfb2f223445
Obasi Ikechukwu Thompson <obasiikechukwu761@gmail.com>
2024-02-25 13:55:34 +0100

    Fixed malfunctioning Nav menu

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 let nav = document.getElementById("show");
 let mt = document.getElementById("home");
+let portpage = document.getElementById("portfo");
+console.log(portpage)
 function showMenu(){
     nav.classList.remove("hide-navBar");
     nav.classList.add("show-navBar");
     mt.classList.remove("mt"); 
+}
+function showPoMenu(){
+    nav.classList.remove("hide-navBar");
+    nav.classList.add("show-navBar");
+    portpage.classList.remove("mt"); 
 }
 
 function hideMenu(){
     nav.classList.remove("show-NavBar");
     nav.classList.add("hide-navBar");
     mt.classList.add("mt");
+    portpage.classList.add("mt");
 }
 
 //Running a loop to input hobbies
